1a01eda1196460e9fd190ddc11db6cebcc00428b
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-12-26 02:19:19 +0530

    Use mediarecorder to save video

--- app/src/main/java/com/utkarshsinha/stablefeather/OpencvCameraView.java
+++ app/src/main/java/com/utkarshsinha/stablefeather/OpencvCameraView.java
@@ -1,17 +1,23 @@
 package com.utkarshsinha.stablefeather;
 
 import android.content.Context;
-import android.graphics.drawable.GradientDrawable;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.util.Log;
 
 import org.opencv.android.JavaCameraView;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Created by utkarsh on 12/25/14.
  */
 public class OpencvCameraView extends JavaCameraView {
+    protected static final String TAG = "SFOCV::CameraView";
+    protected MediaRecorder mMediaRecorder;
+
     OpencvCameraView(Context ctx, int id) {
         super(ctx, id);
     }
@@ -25,9 +31,9 @@ public class OpencvCameraView extends JavaCameraView {
     }
 
     private void tweakParameters() {
-        // So - we need a fixed focal length.
+        // Set the focus to infinity. We still don't know the focal length, though
         Camera.Parameters params = mCamera.getParameters();
-        params.setFocusMode(Camera.Parameters.FOCUS_MODE_FIXED);
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
         mCamera.setParameters(params);
     }
 
@@ -53,5 +59,79 @@ public class OpencvCameraView extends JavaCameraView {
         return mCamera.getParameters().getSupportedPreviewSizes();
     }
 
+    /**
+     * Sets up the appropriate objects to start recordingvideo
+     */
+    protected void prepareVideoRecorder() {
+        CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+        profile.videoFrameHeight = mFrameHeight;
+        profile.videoFrameWidth = mFrameWidth;
 
+        mMediaRecorder = null;
+        mMediaRecorder = new MediaRecorder();
+        mCamera.unlock();
+
+        mMediaRecorder.setCamera(mCamera);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mMediaRecorder.setOutputFormat(profile.fileFormat);
+        mMediaRecorder.setVideoFrameRate(profile.videoFrameRate);
+        mMediaRecorder.setVideoSize(mFrameHeight, mFrameWidth);
+        mMediaRecorder.setVideoEncodingBitRate(profile.videoBitRate);
+        mMediaRecorder.setVideoEncoder(profile.videoCodec);
+    }
+
+    /**
+     * Starts recording video from the camera. It automatically creates a
+     * media recorder object and saves the video to disk.
+     * @param filename The file where the video will be stored.
+     * @return if recording the video was successfully initiated or not.
+     */
+    public boolean recordVideo(String filename) {
+        if(mMediaRecorder!=null) {
+            // Something's already being recorded apparently
+            return false;
+        }
+
+        prepareVideoRecorder();
+        mMediaRecorder.setOutputFile(filename);
+
+        try {
+            mMediaRecorder.prepare();
+        }
+        catch (IllegalStateException e) {
+            Log.e(TAG, "IllegalStateException while preparing media recorder");
+            return false;
+        }
+        catch (IOException e) {
+            Log.e(TAG, "IOException while preparing media recorder");
+            return false;
+        }
+
+        // Start the recording!
+        mMediaRecorder.start();
+        return true;
+    }
+
+    public boolean stopRecord() {
+        if(mMediaRecorder==null) {
+            // Nothing's being recorded apparently
+            return false;
+        }
+
+        // Stop recording, release everything and return true!
+        mMediaRecorder.stop();
+        releaseMediaRecorder();
+
+        return true;
+    }
+
+    protected void releaseMediaRecorder() {
+        if(mMediaRecorder!=null) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+            mCamera.lock();
+        }
+    }
 }
